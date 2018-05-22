e676bd411aa22f4a335b3625e283c958262abbb1
Manoj Panta <manojpanta95@gmail.com>
2018-05-22 10:02:06 -0600

    added admin controller and limitations to normal user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180521155535) do
+ActiveRecord::Schema.define(version: 20180522152223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180521155535) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "role"
   end
 
   add_foreign_key "actor_movies", "actors"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 describe User, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:username)}
-    it {should validate_uniqueness_of(:username)}
-    it {should validate_presence_of(:password)}
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password) }
+  end
+
+  describe 'roles' do
+    it 'can be created as admin' do
+      user = User.create(username: 'manoj', password: 'manoj1', role: 1)
+
+      expect(user.role).to eq('admin')
+      expect(user.admin?).to be_truthy
+    end
+    it 'can be created as default user' do
+      user = User.create(username: 'manoj', password: 'manoj1', role: 0)
+
+      expect(user.role).to eq('default')
+      expect(user.default?).to be_truthy
+    end
   end
 end
